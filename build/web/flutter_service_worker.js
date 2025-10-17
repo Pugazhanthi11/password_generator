74a54e51d5c5d3eea844bf09cc9af0fdc70238a5
Pugazhanthi V K <pugazhanthi.vk@gmail.com>
2025-10-17 10:46:27 +0530

    Clean build for deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "00d3d74e44f73ccf9ea6f4e9fe856755
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9b70e79d28821311be470d6001b4b5eb",
+"flutter_bootstrap.js": "9ae4a9fa2e4cf6b17db22ad9a8925603",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
